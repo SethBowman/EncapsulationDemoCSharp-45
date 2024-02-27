b2306bdc8a712bd33ad9897e45e9107dffbeeed8
Seth Bowman <brandonsethbowman@gmail.com>
2024-02-27 14:22:29 -0600

    Add project files.

---
+++ EncapsulationDemoCSharp-45/Program.cs
@@ -0,0 +1,15 @@
+﻿using EncapsulationDemoCSharp_45;
+
+var seth = new Person();
+
+seth.AddMoney(200.00);
+
+var currentWallet = seth.CheckWallet();
+
+Console.WriteLine(currentWallet);
+
+seth.AddMoney(1000.00);
+
+currentWallet = seth.CheckWallet();
+
+Console.WriteLine(currentWallet);
